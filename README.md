3446f15df3645a360bc45207cee765f1d61048b2
Tim S. Van Haren <tsvanharen@gmail.com>
2024-02-12 22:17:58 -0500

    docs: mostly docs, but also added some core data

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # dataremix.us
 
-Data-driven insights into the human experience.
+Data-driven insights into the modern human experience.
+
+## Hosting
+
+* Registrar and DNS management by Cloudflare (not using its proxy at this time)
+* Deployed with GitHub Actions
+* Hosted at GitHub Pages
+
+## Links to things that helped get this site setup
+
+* Next.js GitHub Pages (https://github.com/gregrickaby/nextjs-github-pages/tree/main)
+* Managing a custom domain for your GitHub Pages site (https://docs.github.com/en/pages/configuring-a-custom-domain-for-your-github-pages-site/managing-a-custom-domain-for-your-github-pages-site)
+* One A record pointing to multiple IPs (https://community.cloudflare.com/t/one-a-record-pointing-to-multiple-ips/249887/3)
+* Convert CSV to JSON (https://csvjson.com/csv2json)
+
+## Roadmap
+
+Will shift to its own page eventually.  Just getting some notes documented.
+
+* Template (fonts, branding, styling, layouts)
+* Add copyright notice in footer
+* First insight:  "How likely is it that your home will broken into?"
+* Document data sets used, any links used for data, and be clear on methodology for each insight.  Annotation is table stakes for this endeavor.
+  * Dataset:  housing unit estimates by state, July 1, 2022:  https://www.census.gov/data/datasets/time-series/demo/popest/2020s-total-housing-units.html#v2022
+  * Dataset:  burgleries by state for 2022:  https://cde.ucr.cjis.gov/LATEST/webapp/#/pages/explorer/crime/query
+  * Dataset:  population by state for 2022:  https://www.census.gov/data/tables/time-series/demo/popest/2020s-state-total.html#v2022
---
+++ src/data/index.ts
@@ -0,0 +1,321 @@
+type Data = State[]
+
+type State = {
+  name: string;
+  housingUnits: number;
+  burglaries: number;
+  population: number;
+}
+
+// generated from pasting the data from 2022-07-01-estimates-by-state.csv into https://csvjson.com/csv2json,
+// then modified the keys to allow easier creation of usable types
+const data: Data = [
+  {
+    name: "Alabama",
+    housingUnits: 2339582,
+    burglaries: 14408,
+    population: 5074296
+  },
+  {
+    name: "Alaska",
+    housingUnits: 329285,
+    burglaries: 1973,
+    population: 733583
+  },
+  {
+    name: "Arizona",
+    housingUnits: 3186612,
+    burglaries: 19941,
+    population: 7359197
+  },
+  {
+    name: "Arkansas",
+    housingUnits: 1395709,
+    burglaries: 14204,
+    population: 3045637
+  },
+  {
+    name: "California",
+    housingUnits: 14627460,
+    burglaries: 145141,
+    population: 39029342
+  },
+  {
+    name: "Colorado",
+    housingUnits: 2591780,
+    burglaries: 23077,
+    population: 5839926
+  },
+  {
+    name: "Connecticut",
+    housingUnits: 1540363,
+    burglaries: 4716,
+    population: 3626205
+  },
+  {
+    name: "Delaware",
+    housingUnits: 465787,
+    burglaries: 2068,
+    population: 1018396
+  },
+  {
+    name: "District of Columbia",
+    housingUnits: 360890,
+    burglaries: 1352,
+    population: 671803
+  },
+  {
+    name: "Florida",
+    housingUnits: 10257426,
+    burglaries: 38594,
+    population: 22244823
+  },
+  {
+    name: "Georgia",
+    housingUnits: 4539156,
+    burglaries: 23772,
+    population: 10912876
+  },
+  {
+    name: "Hawaii",
+    housingUnits: 568075,
+    burglaries: 3745,
+    population: 1440196
+  },
+  {
+    name: "Idaho",
+    housingUnits: 796958,
+    burglaries: 3075,
+    population: 1939033
+  },
+  {
+    name: "Illinois",
+    housingUnits: 5452765,
+    burglaries: 26229,
+    population: 12582032
+  },
+  {
+    name: "Indiana",
+    housingUnits: 2977293,
+    burglaries: 15449,
+    population: 6833037
+  },
+  {
+    name: "Iowa",
+    housingUnits: 1438565,
+    burglaries: 6997,
+    population: 3200517
+  },
+  {
+    name: "Kansas",
+    housingUnits: 1292622,
+    burglaries: 8029,
+    population: 2937150
+  },
+  {
+    name: "Kentucky",
+    housingUnits: 2023724,
+    burglaries: 11044,
+    population: 4512310
+  },
+  {
+    name: "Louisiana",
+    housingUnits: 2113216,
+    burglaries: 22850,
+    population: 4590241
+  },
+  {
+    name: "Maine",
+    housingUnits: 751782,
+    burglaries: 1605,
+    population: 1385340
+  },
+  {
+    name: "Maryland",
+    housingUnits: 2559195,
+    burglaries: 11728,
+    population: 6164660
+  },
+  {
+    name: "Massachusetts",
+    housingUnits: 3036334,
+    burglaries: 9968,
+    population: 6981974
+  },
+  {
+    name: "Michigan",
+    housingUnits: 4611660,
+    burglaries: 21503,
+    population: 10034113
+  },
+  {
+    name: "Minnesota",
+    housingUnits: 2547955,
+    burglaries: 12244,
+    population: 5717184
+  },
+  {
+    name: "Mississippi",
+    housingUnits: 1342831,
+    burglaries: 10296,
+    population: 2940057
+  },
+  {
+    name: "Missouri",
+    housingUnits: 2826508,
+    burglaries: 18276,
+    population: 6177957
+  },
+  {
+    name: "Montana",
+    housingUnits: 529152,
+    burglaries: 2249,
+    population: 1122867
+  },
+  {
+    name: "Nebraska",
+    housingUnits: 863913,
+    burglaries: 3591,
+    population: 1967923
+  },
+  {
+    name: "Nevada",
+    housingUnits: 1328725,
+    burglaries: 13859,
+    population: 3177772
+  },
+  {
+    name: "New Hampshire",
+    housingUnits: 648567,
+    burglaries: 1025,
+    population: 1395231
+  },
+  {
+    name: "New Jersey",
+    housingUnits: 3785346,
+    burglaries: 13708,
+    population: 9261699
+  },
+  {
+    name: "New Mexico",
+    housingUnits: 956778,
+    burglaries: 12764,
+    population: 2113344
+  },
+  {
+    name: "New York",
+    housingUnits: 8586228,
+    burglaries: 31976,
+    population: 19677151
+  },
+  {
+    name: "North Carolina",
+    housingUnits: 4892662,
+    burglaries: 39668,
+    population: 10698973
+  },
+  {
+    name: "North Dakota",
+    housingUnits: 377771,
+    burglaries: 2789,
+    population: 779261
+  },
+  {
+    name: "Ohio",
+    housingUnits: 5293356,
+    burglaries: 30015,
+    population: 11756058
+  },
+  {
+    name: "Oklahoma",
+    housingUnits: 1776666,
+    burglaries: 19401,
+    population: 4019800
+  },
+  {
+    name: "Oregon",
+    housingUnits: 1859387,
+    burglaries: 15295,
+    population: 4240137
+  },
+  {
+    name: "Pennsylvania",
+    housingUnits: 5815392,
+    burglaries: 19231,
+    population: 12972008
+  },
+  {
+    name: "Rhode Island",
+    housingUnits: 486029,
+    burglaries: 1401,
+    population: 1093734
+  },
+  {
+    name: "South Carolina",
+    housingUnits: 2446724,
+    burglaries: 18632,
+    population: 5282634
+  },
+  {
+    name: "South Dakota",
+    housingUnits: 408306,
+    burglaries: 2393,
+    population: 909824
+  },
+  {
+    name: "Tennessee",
+    housingUnits: 3144618,
+    burglaries: 22499,
+    population: 7051339
+  },
+  {
+    name: "Texas",
+    housingUnits: 12136678,
+    burglaries: 100402,
+    population: 30029572
+  },
+  {
+    name: "Utah",
+    housingUnits: 1228746,
+    burglaries: 6818,
+    population: 3380800
+  },
+  {
+    name: "Vermont",
+    housingUnits: 339034,
+    burglaries: 984,
+    population: 647064
+  },
+  {
+    name: "Virginia",
+    housingUnits: 3685322,
+    burglaries: 10818,
+    population: 8683619
+  },
+  {
+    name: "Washington",
+    housingUnits: 3314390,
+    burglaries: 43837,
+    population: 7785786
+  },
+  {
+    name: "West Virginia",
+    housingUnits: 861633,
+    burglaries: 3377,
+    population: 1775156
+  },
+  {
+    name: "Wisconsin",
+    housingUnits: 2770583,
+    burglaries: 9059,
+    population: 5892539
+  },
+  {
+    name: "Wyoming",
+    housingUnits: 277116,
+    burglaries: 1218,
+    population: 581381
+  }
+]
+
+export default data;
